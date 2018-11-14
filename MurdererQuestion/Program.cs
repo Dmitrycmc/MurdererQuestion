921308429bce8131c90ec9ca5c4ef0ac13425581
Dmitry Litov <Dmitrycmc@gmail.com>
2018-11-14 04:07:09 +0300

    Uncorrect model

--- MurdererQuestion/Program.cs
+++ MurdererQuestion/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.ML.Probabilistic.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,27 @@ namespace MurdererQuestion
 	{
 		static void Main(string[] args)
 		{
+			var MurdererIsGray = Variable.Bernoulli(0.3);
+			var WeaponIsRevolver = Variable.New<bool>();
+			var FoundGraysHair = Variable.New<bool>();
+
+			using (Variable.If(MurdererIsGray))
+			{
+				WeaponIsRevolver = Variable.Bernoulli(0.9);
+				FoundGraysHair = Variable.Bernoulli(0.5); 
+			}
+
+			using (Variable.IfNot(MurdererIsGray))
+			{
+				WeaponIsRevolver = Variable.Bernoulli(0.2);
+				FoundGraysHair = Variable.Bernoulli(0.05);
+			}
+
+			InferenceEngine engine = new InferenceEngine();
+			WeaponIsRevolver.ObservedValue = true;
+			FoundGraysHair.ObservedValue = true;
+			Console.WriteLine("MurdererIsGray: " + engine.Infer(MurdererIsGray));
+
 		}
 	}
 }
