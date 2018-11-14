02d8ce73efe8b93f267f53e243adc56a7d9b79c8
Dmitry Litov <Dmitrycmc@gmail.com>
2018-11-14 12:44:52 +0300

    Correct model, Added factor graph

--- MurdererQuestion/Program.cs
+++ MurdererQuestion/Program.cs
@@ -11,27 +11,36 @@ namespace MurdererQuestion
 	{
 		static void Main(string[] args)
 		{
-			var MurdererIsGray = Variable.Bernoulli(0.3);
-			var WeaponIsRevolver = Variable.New<bool>();
-			var FoundGraysHair = Variable.New<bool>();
+			var MurdererIsGray = Variable.Bernoulli(0.7).Named("MurdererIsGray");
+			var WeaponIsRevolver = Variable.New<bool>().Named("WeaponIsRevolver");
+			var FoundGraysHair = Variable.New<bool>().Named("FoundGraysHair");
+			var Found2ndHair = Variable.New<bool>().Named("Found2ndHair");
 
 			using (Variable.If(MurdererIsGray))
 			{
-				WeaponIsRevolver = Variable.Bernoulli(0.9);
-				FoundGraysHair = Variable.Bernoulli(0.5); 
+				WeaponIsRevolver.SetTo(Variable.Bernoulli(0.9));
+				FoundGraysHair.SetTo(Variable.Bernoulli(0.5));
+				Found2ndHair.SetTo(Variable.Bernoulli(0.05));
 			}
 
 			using (Variable.IfNot(MurdererIsGray))
 			{
-				WeaponIsRevolver = Variable.Bernoulli(0.2);
-				FoundGraysHair = Variable.Bernoulli(0.05);
+				WeaponIsRevolver.SetTo(Variable.Bernoulli(0.2));
+				FoundGraysHair.SetTo(Variable.Bernoulli(0.05));
+				Found2ndHair.SetTo(Variable.Bernoulli(0.5));
 			}
 
 			InferenceEngine engine = new InferenceEngine();
-			WeaponIsRevolver.ObservedValue = true;
-			FoundGraysHair.ObservedValue = true;
+			WeaponIsRevolver.ObservedValue = false;
+			FoundGraysHair.ObservedValue = false;
+			Found2ndHair.ObservedValue = true;
+			InferenceEngine.Visualizer = new Microsoft.ML.Probabilistic.Compiler.Visualizers.WindowsVisualizer();
+			engine.ShowFactorGraph = true;
 			Console.WriteLine("MurdererIsGray: " + engine.Infer(MurdererIsGray));
-
+			
+			/// 1 фактор граф
+			/// 2 продемострировать примеры
+			/// 3 написать линейную регрессию
 		}
 	}
 }
